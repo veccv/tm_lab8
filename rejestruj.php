cbe1ab1f15598d1b3a72df10edbe359a5cc13f96
Jacek <jacdal000@pbs.edu.pl>
2023-01-04 12:59:51 +0100

    Added employee registration

---
+++ rejestruj.php
@@ -0,0 +1,19 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl"> <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> </head>
+<BODY>
+Formularz rejestracji
+<form method="post" action="add_user.php">
+    Login:
+    <input type="text" name="user" maxlength="20" size="20">
+    <br> Hasło:
+    <input type="password" name="pass" maxlength="20" size="20">
+    <br> Powtórz hasło:
+    <input type="password" name="pass_repeat" maxlength="20" size="20">
+    <br> <input type="submit" value="Rejestruj"/>
+</form>
+<br>
+<br>
+<br>
+<br>
+<a href="index.php">Powrót do menu głównego aplikacji</a>
+</BODY> </HTML>
