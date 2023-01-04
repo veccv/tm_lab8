489837e688dbc7b0b399ff7a9ca5cb976fa9239f
Jacek Dalmer <jacdal000@pbs.edu.pl>
2023-01-04 12:42:19 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<head>
+    <title>
+        Dalmer
+    </title>
+</head>
+<body>
+<?php
+session_start();
+session_unset();
+?>
+<b>Zadanie 8. E-learning</b>
+<br>
+<a href="rejestruj.php">Rejestracja</a>
+<br>
+<a href="index4.php">Logowanie / panel główny</a>
