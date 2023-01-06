17745370226de2efe9898f784ae4e72ae167e7ad
Jacek <jacdal000@pbs.edu.pl>
2023-01-06 14:32:34 +0100

    Added blocking lessons

--- lesson_view.php
+++ lesson_view.php
@@ -33,45 +33,51 @@ $topic_id = $_GET['id'];
 $role = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM users WHERE login='$user'"))[3];
 
 echo '<a href="index4.php">Powrót do wyboru lekcji</a><br><br>';
-$lesson_text = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM lessons WHERE id='$topic_id'"))[3];
-$lesson_author_id = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM lessons WHERE id='$topic_id'"))[1];
-$lesson_author_name = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM users WHERE id='$lesson_author_id'"))[1];
-$lesson_file = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM lessons WHERE id='$topic_id'"))[4];
 
-echo '<table class="table table-bordered table-striped">';
-echo '<thead>';
-echo '<tr>';
-echo '<th>Informacje</th>';
-echo '<th>Lekcja</th>';
-echo '</tr>';
-echo '</thead>';
-echo '<tbody>';
-echo '<tr>';
-echo '<td>Autor lekcji: ' . $lesson_author_name . '</td>';
-echo '<td>' . $lesson_text . '<br><br>';
+$blocked = mysqli_fetch_array(Database::getConnection()->query("SELECT blocked FROM lessons WHERE id='$topic_id'"))[0];
+if ($blocked == 'no') {
+    $lesson_text = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM lessons WHERE id='$topic_id'"))[3];
+    $lesson_author_id = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM lessons WHERE id='$topic_id'"))[1];
+    $lesson_author_name = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM users WHERE id='$lesson_author_id'"))[1];
+    $lesson_file = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM lessons WHERE id='$topic_id'"))[4];
 
-if (strlen($lesson_file) > 0) {
-    $message = 'lekcje/' . $lesson_file;
+    echo '<table class="table table-bordered table-striped">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th>Informacje</th>';
+    echo '<th>Lekcja</th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+    echo '<tr>';
+    echo '<td>Autor lekcji: ' . $lesson_author_name . '</td>';
+    echo '<td>' . $lesson_text . '<br><br>';
 
-    if (strpos($message, '.png')) {
-        $message = "<img src='$message'>";
-    } else if (strpos($message, '.gif')) {
-        $message = "<img src='$message'>";
-    } else if (strpos($message, '.jpg')) {
-        $message = "<img src='$message'>";
-    } else if (strpos($message, '.mp3')) {
-        $message = "<audio controls src='$message' autoplay='true'> </audio>";
-    } else if (strpos($message, '.mp4')) {
-        $message = "<video controls width='250' autoplay='true'><source src='$message' type='video/mp4'></video>";
+    if (strlen($lesson_file) > 0) {
+        $message = 'lekcje/' . $lesson_file;
+
+        if (strpos($message, '.png')) {
+            $message = "<img src='$message'>";
+        } else if (strpos($message, '.gif')) {
+            $message = "<img src='$message'>";
+        } else if (strpos($message, '.jpg')) {
+            $message = "<img src='$message'>";
+        } else if (strpos($message, '.mp3')) {
+            $message = "<audio controls src='$message' autoplay='true'> </audio>";
+        } else if (strpos($message, '.mp4')) {
+            $message = "<video controls width='250' autoplay='true'><source src='$message' type='video/mp4'></video>";
+        }
+        echo $message;
     }
-    echo $message;
+    echo '</td>';
+    echo '</tr>';
+
+
+    echo '</tbody>';
+    echo '</table>';
+} else {
+    echo 'Ta lekcja jest zablokowana!';
 }
-echo '</td>';
-echo '</tr>';
-
-
-echo '</tbody>';
-echo '</table>';
 ?>
 </BODY>
 </HTML>
